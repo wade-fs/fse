f48268730c0f4f20a8df244df035faa4b0e555af
wade <wade.fs@gmail.com>
2026-06-29 15:46:20 +0800

    抽離 object, entity

--- adventures/pc/mudlib/std/object.c
+++ adventures/pc/mudlib/std/object.c
@@ -1,17 +1,6 @@
 // /std/object.c  (史前文明 PC)
-// 最基本的屬性容器，與 runtime/core/object.c 同構
-private mapping temp_data;
-private mapping dbase;
+inherit "/runtime/core/object";
 
 void create() {
-    temp_data = ([]);
-    dbase = ([]);
+    ::create();
 }
-
-void set(string prop, mixed val)   { dbase[prop] = val; }
-mixed query(string prop)           { return dbase[prop]; }
-void delete(string prop)           { map_delete(dbase, prop); }
-
-void set_temp(string prop, mixed val) { temp_data[prop] = val; }
-mixed query_temp(string prop)         { return temp_data[prop]; }
-void delete_temp(string prop)         { map_delete(temp_data, prop); }
--- adventures/pw/mudlib/std/entity.c
+++ adventures/pw/mudlib/std/entity.c
@@ -1,16 +1,6 @@
-// /runtime/core/entity.c
-// FSE 核心最基本實體基底
-inherit "/std/object";
-
-private string entity_id;
-private string entity_type;
+// /std/entity.c  (PW)
+inherit "/runtime/core/entity";
 
 void create() {
     ::create();
 }
-
-void set_entity_id(string id) { entity_id = id; }
-string query_entity_id() { return entity_id; }
-
-void set_entity_type(string type) { entity_type = type; }
-string query_entity_type() { return entity_type; }
